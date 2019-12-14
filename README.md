e1e97efeed7eae258362218d67c85856942421b6
Jay Colson <jay@karma.net>
2019-12-14 13:32:32 +0000

    added ability to convert banktivity csvs to quicken (mint)

---
+++ README.md
@@ -0,0 +1,11 @@
+# open24tomint
+
+This app is primarily used to convert csv files from the permanent tsb (open24.ie) to the mint csv format,
+which is importable into Quicken.
+
+It also allows the conversion of banktivity csv exports into mint csv format for importing into quicken.
+
+how to build:
+
+go build
+
--- open24tomint.go
+++ open24tomint.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -12,21 +13,83 @@ import (
 	"time"
 )
 
+var helpFlag = flag.Bool("h", false, "Help")
+var csvInputFlag = flag.String("input", "examplepermtsb.csv", "The csv input file to read")
+var inputTypeFlag = flag.String("type", "open24", "Input format type.  Available options: open24, banktivity")
+
 func check(err error) {
 	if err != nil {
+		flag.PrintDefaults()
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("need to pass in the name of the csv file")
+	flag.Parse()
+	if *helpFlag {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	filename := os.Args[1]
-	csvStuff(filename)
+	filename := *csvInputFlag
+	if *inputTypeFlag == "open24" {
+		open24CsvStuff(filename)
+	} else if *inputTypeFlag == "banktivity" {
+		banktivityCsvStuff(filename)
+	}
 }
 
-func csvStuff(filename string) {
+func banktivityCsvStuff(filename string) {
+	inLayout := "1/2/06"
+	outLayout := "01/02/2006"
+	f, err := os.Open(filename)
+	check(err)
+	fileReader := bufio.NewReader(f)
+	r := csv.NewReader(fileReader)
+	//read header line first and ignore it
+	r.Read()
+	fmt.Println("Date,Payee,FI Payee,Amount,CreditDebit,Category")
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		check(err)
+		date, err := time.Parse(inLayout, record[2])
+		check(err)
+		formattedDate := date.Format(outLayout)
+		creditDebit := string([]rune(record[5])[0])
+		currency := "$"
+		amount := float64(0)
+		if creditDebit == "-" {
+			currency = string([]rune(record[5])[1])
+			creditDebit = "debit"
+			amountString := string([]rune(record[5])[2:])
+			amount, err = strconv.ParseFloat(strings.Replace(amountString, ",", "", -1), 64)
+		} else {
+			currency = creditDebit
+			creditDebit = "credit"
+			amountString := string([]rune(record[5])[1:])
+			amount, err = strconv.ParseFloat(strings.Replace(amountString, ",", "", -1), 64)
+		}
+		// fmt.Printf("currency %s\n", currency)
+		fmt.Printf("\"")
+		fmt.Printf(formattedDate)
+		fmt.Printf("\",\"")
+		fmt.Printf(record[3])
+		fmt.Printf("\",\"")
+		fmt.Printf("%s - %s", record[6], currency)
+		fmt.Printf("\",\"")
+		fmt.Printf("%f", amount)
+		fmt.Printf("\",\"")
+		fmt.Printf(creditDebit)
+		fmt.Printf("\",\"")
+		fmt.Printf(record[4])
+		fmt.Printf("\"")
+		fmt.Printf("\n")
+	}
+}
+
+func open24CsvStuff(filename string) {
 	inLayout := "02 Jan 06"
 	outLayout := "01/02/2006"
 	f, err := os.Open(filename)
@@ -35,7 +98,7 @@ func csvStuff(filename string) {
 	r := csv.NewReader(fileReader)
 	//read header line first and ignore it
 	r.Read()
-	fmt.Println("Date,Payee,FI Payee,Amount,Memo?,Category")
+	fmt.Println("Date,Payee,FI Payee,Amount,CreditDebit,Category")
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -51,6 +114,7 @@ func csvStuff(filename string) {
 			creditDebit = "credit"
 		}
 		moneyIn, err := strconv.ParseFloat(strings.Replace(record[2], ",", "", -1), 64)
+		amount := moneyIn - moneyOut
 		fmt.Printf("\"")
 		fmt.Printf(formattedDate)
 		fmt.Printf("\",\"")
@@ -58,12 +122,11 @@ func csvStuff(filename string) {
 		fmt.Printf("\",\"")
 		fmt.Printf(record[1])
 		fmt.Printf("\",\"")
-		fmt.Printf("%f", moneyIn-moneyOut)
+		fmt.Printf("%f", amount)
 		fmt.Printf("\",\"")
 		fmt.Printf(creditDebit)
 		fmt.Printf("\",\"")
 		fmt.Printf("\"")
 		fmt.Printf("\n")
 	}
-
 }
